3023730815590a1164ffa0259cf36a2fba592023
Talha7447 <talhashinwari7474@gmail.com>
2024-04-06 15:08:32 +0500

    fix filtering projects

--- resources/js/Components/SelectInput.jsx
+++ resources/js/Components/SelectInput.jsx
@@ -1,18 +1,19 @@
-import { forwardRef } from 'react';
+import { forwardRef, useRef } from 'react';
 
-const SelectInput = forwardRef(({ className = '', children, ...props }, ref) => {
+export default forwardRef(function SelectInput({ className = '', children, ...props }, ref) {
+    const input = ref ? ref : useRef();
+
     return (
         <select
             {...props}
+
             className={
                 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
                 className
             }
-            ref={ref}
+            ref={input}
         >
             {children}
-        </select>
+            </select>
     );
 });
-
-export default SelectInput;
